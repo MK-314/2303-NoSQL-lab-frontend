92cbd06da5ff9e36cbffb4f93d1c2a45da9f1b13
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2022-04-13 22:36:45 -0700

    update

--- src/datahandler.js
+++ src/datahandler.js
@@ -1,11 +1,10 @@
 class FetchApi {
     static checkAuth(authObject, onSuccess, onError) {
-        fetch(`http://192.168.63.77:8080/auth/login?username=${authObject.username}&${authObject.password}`, {
+        fetch(`http://192.168.63.77:8080/auth/login?username=${authObject.username}&password=${authObject.password}`, {
                 'method': 'GET',
                 'headers': {
                     'Content-Type': 'application/json'
-                },
-                'body': JSON.stringify(authObject)
+                }
             })
             .then(responce => responce.json())
             .then(data => onSuccess(data.message))
